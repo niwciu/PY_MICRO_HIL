b9c0383a8b0003fb95d031db0ab513fed82730e4
niwciu <niwciu@gmail.com>
2025-06-21 23:28:20 +0200

    framework_API refactored and remove one critical bug

--- src/py_micro_hil/framework_API.py
+++ src/py_micro_hil/framework_API.py
@@ -1,51 +1,62 @@
 from py_micro_hil.assertions import _current_framework
-from py_micro_hil.RPiPeripherals import RPiUART_API , RPiGPIO_API, RPiPWM_API, RPiI2C_API, RPiSPI_API, RPi1Wire_API, RPiADC_API, RPiCAN_API, RPiHardwarePWM_API
+from py_micro_hil.RPiPeripherals import (
+    RPiUART_API, RPiGPIO_API, RPiPWM_API, RPiI2C_API,
+    RPiSPI_API, RPi1Wire_API, RPiADC_API, RPiCAN_API, RPiHardwarePWM_API
+)
 from py_micro_hil.protocols import ModbusRTU_API
+
+
 def _get_framework():
     framework = _current_framework.get()
     if not framework:
         raise RuntimeError("No framework context is set for this test")
     return framework
 
+
 def get_ModBus_peripheral() -> ModbusRTU_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("protocols", "ModbusRTU")
+
 
 def get_RPiUART_peripheral() -> RPiUART_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiUART")
 
+
 def get_RPiGPIO_peripheral() -> RPiGPIO_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiGPIO")
 
+
 def get_RPiPWM_peripheral() -> RPiPWM_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiPWM")
+
 
 def get_RPiI2C_peripheral() -> RPiI2C_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiI2C")
 
-def get_RPiPWM_peripheral() -> RPiPWM_API:
-    framework = _get_framework()
-    return framework.peripheral_manager.get_device("peripherals", "RPiPWM")
 
-def get_RPiPWM_peripheral() -> RPiSPI_API:
+def get_RPiSPI_peripheral() -> RPiSPI_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiSPI")
+
 
 def get_RPiCAN_peripheral() -> RPiCAN_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiCAN")
 
+
 def get_RPiADC_peripheral() -> RPiADC_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPiADC")
 
+
 def get_RPi1Wire_peripheral() -> RPi1Wire_API:
     framework = _get_framework()
     return framework.peripheral_manager.get_device("peripherals", "RPi1Wire")
+
 
 def get_RPiHardwarePWM_peripheral() -> RPiHardwarePWM_API:
     framework = _get_framework()
